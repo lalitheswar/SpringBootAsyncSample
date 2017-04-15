1d078123923622ec38d3f9802c7031f70acb0bbe
lalitheswar <lalitheswar4@gmail.com>
2017-04-16 01:23:09 +0800

    fix the async method

--- src/main/java/com/al/app/SpringBootAsyncApplication.java
+++ src/main/java/com/al/app/SpringBootAsyncApplication.java
@@ -1,8 +1,13 @@
 package com.al.app;
 
+import java.util.concurrent.Executor;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 /**
  * @author lalith
@@ -10,8 +15,20 @@ import org.springframework.context.annotation.ComponentScan;
  */
 @EnableAutoConfiguration
 @ComponentScan("com.al.app.*")
-public class SpringBootAsyncApplication {
+@EnableAsync
+public class SpringBootAsyncApplication extends AsyncConfigurerSupport {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootAsyncApplication.class, args);
+    }
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(2);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("SampleAsync-");
+        executor.initialize();
+        return executor;
     }
 }
